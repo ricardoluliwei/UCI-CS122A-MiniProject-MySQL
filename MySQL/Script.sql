171e5143fcf589159a365ab08cf34d402ab9d35f
ricardo <ricardoluliwei@gmail.com>
2020-03-16 00:12:58 -0700

    Update Script.sql

--- MySQL/Script.sql
+++ MySQL/Script.sql
@@ -2,24 +2,25 @@ use blurts;
 
 #1
 select t.id, t.description, count( b.blurtid)
-from topic as t, blurt_analysis as b
-where t.id = b.topicid
+from topic as t left join blurt_analysis as b
+on t.id = b.topicid
 group by t.id
 order by t.id;
 
 
 #2
 select u.name, COUNT(f.follower)
-from celebrity as c, follow as f, user as u
-where c.email = f.followee and c.email = u.email
-group by u.name, c.email
+from user as u left join follow as f
+on u.email = f.followee
+where u.type = 'C'
+group by u.email
 order by u.name;
 
 
 #3
 select u.name, COUNT(b.blurtid)
-from celebrity as c, blurt as b, user as u
-where c.email = b.email and c.email = u.email
+from celebrity as c left join blurt as b on c.email = b.email, user as u
+where c.email = u.email
 group by c.email
 order by COUNT(b.blurtid) desc;
 
@@ -43,38 +44,49 @@ order by v.name, a.email;
 
 #6
 select v.name, count(distinct(b.email)) as advertisement_gap
-from blurt_analysis as b, topic as t, vendor_topics as vt, vendor as v
-where v.id = vt.vendorid and b.topicid = t.id and t.id = vt.topicid and b.email not in
+from blurt_analysis as b, vendor_topics as vt, vendor as v
+where v.id = vt.vendorid and b.topicid = vt.topicid and b.email not in
 (select ua.email
 from user_ad as ua, advertisement as a
-where ua.adid = a.id and a.vendorid = vt.topicid)
+where ua.adid = a.id and a.vendorid = vt.vendorid)
 group by v.name
-order by v.name;
+order by count(distinct(b.email)) desc;
 
 
 #7
 select distinct u1.name, u2.name
-from user as u1, blurt_analysis as ba1, user as u2, blurt_analysis ba2
-where u1.email = ba1.email and u1.email <> u2.email and u2.email = ba2.email
-and ba1.topicid = ba2.topicid and not exists
-(select f.follower
+from user as u1, user as u2
+where u1.email <> u2.email and
+exists 
+(select ba.topicid
+from blurt_analysis as ba
+where ba.email = u1.email
+and ba.topicid in
+(select ba1.topicid
+from blurt_analysis as ba1
+where ba1.email = u2.email))
+and not exists
+(select *
 from follow as f
 where f.followee = u2.email and f.follower = u1.email)
 order by u1.name, u2.name;
 
 #8
-select u1.name, u2.name, u3.name
+select distinct u1.email, u2.email, u3.email
 from user as u1, follow as f1,user as u2, follow as f2, user as u3
 where u1.email = f1.follower and u2.email = f1.followee
 and u2.email = f2.follower and u3.email = f2.followee
-and u3.email <> f1.followee
-order by u1.name, u2.name, u3.name;
+and u1.email <> u3.email and u1.email not in
+(select u4.email
+from user as u4, follow as f
+where u4.email = f.follower and f.followee = u3.email)
+order by u1.email, u2.email, u3.email;
 
 
 #9
-select t.id, t.description, b.location, count(distinct(b.blurtid)), avg(ba.sentiment)
-from blurt_analysis as ba, blurt as b, topic as t
-where ba.blurtid = b.blurtid and ba.topicid = t.id
+select distinct t.id, t.description, b.location, count(b.blurtid), avg(ba.sentiment)
+from  topic as t left join blurt_analysis as ba natural join blurt as b
+on t.id = ba.topicid
 group by t.id, t.description, b.location
 having avg(ba.sentiment) < 0
 order by t.id;
